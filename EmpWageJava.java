27c03edddad45b911036973a4bd2eb4e014b11e8
dhirajrathod <dhirajrathod3000@gmail.com>
2021-01-24 10:36:11 +0530

    adding to UC2

--- EmpWageJava.java
+++ EmpWageJava.java
@@ -1,21 +1,20 @@
-public class empWageUC1
-{
- 	public static void main(String[] args)
-	{
-		int Result=(int)(Math.random()*10)%2;
-		int isPresent=0;
-	
-		if(Result == isPresent)
-		{
-			System.out.println("Employee is present");
-	
-	        }
+  GNU nano 5.3                                                                           EmpWageJava.java                                                                           Modified
+        public static void main(String[] args)
+        {
+                int Result=(int)(Math.random()*10)%2;
+                int isPresent=0;
 
-	 	else 
-		{
-			System.out.println("Employee is Absent");
-		}           
-		
+                if(Result == isPresent)
+                {
+                        System.out.println("Employee is present");
 
-	}
+                }
+
+                else
+                {
+                        System.out.println("Employee is Absent");
+                }
+
+
+        }
 }
